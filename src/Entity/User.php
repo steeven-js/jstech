72a13d991d87fdf9c08a874fa210970b3aafc809
steeven_js <jacques.steeven@gmail.com>
2022-08-27 10:19:16 -0400

    Création des formulaires Contact

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -134,28 +134,4 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
         return $this;
     }
-
-    public function getSubject(): ?string
-    {
-        return $this->subject;
-    }
-
-    public function setSubject(string $subject): self
-    {
-        $this->subject = $subject;
-
-        return $this;
-    }
-
-    public function getMessage(): ?string
-    {
-        return $this->message;
-    }
-
-    public function setMessage(string $message): self
-    {
-        $this->message = $message;
-
-        return $this;
-    }
 }
