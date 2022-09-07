67d4b9d6039944abeb5089125e2d90963a652c3c
steeven_js <jacques.steeven@gmail.com>
2022-09-06 20:55:18 -0400

    052 Intégration de Stripe dans notre tunnel d'achat

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Category;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class CategoryController extends AbstractController
+{
+     private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+    
+    #[Route('/category', name: 'app_nos_category')]
+    public function idex(): Response
+    {
+        $category = $this->entityManager->getRepository(Category::class)->findAll();
+
+        return $this->render('category/index.html.twig', [
+            'category' => $category
+        ]);
+        
+    }
+    
+    #[Route('/category/{id}', name: 'app_category')]
+    public function show($id): Response
+    {
+        // dd($slug);
+        // On recherche en base de donnée un produit associer à son slug.
+        $category = $this->entityManager->getRepository(Category::class)->findOneById($id);
+
+        $products = $category->getProducts();
+
+        // dd($category);
+
+        return $this->render('category/show.html.twig', [
+            'category' => $category,
+            'products' => $products
+            
+        ]);
+        
+    }
+}
