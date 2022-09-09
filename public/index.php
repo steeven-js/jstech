6d62ba959be9cb55a0835deed3edc06a3f9dd41c
steeven_js <jacques.steeven@gmail.com>
2022-09-09 18:02:56 -0400

    commit for deploy to heroku

--- public/index.php
+++ public/index.php
@@ -4,11 +4,6 @@ use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
-$app->get('/cowsay', function() use($app) {
-  $app['monolog']->addDebug('cowsay');
-  return "<pre>".\Cowsayphp\Cow::say("Cool beans")."</pre>";
-});
-
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
