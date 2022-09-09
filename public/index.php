8050d24c0f32bfbd14e80196762f225ef509e796
steeven_js <jacques.steeven@gmail.com>
2022-09-09 17:40:53 -0400

    Demo

--- public/index.php
+++ public/index.php
@@ -4,6 +4,11 @@ use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
+$app->get('/cowsay', function() use($app) {
+  $app['monolog']->addDebug('cowsay');
+  return "<pre>".\Cowsayphp\Cow::say("Cool beans")."</pre>";
+});
+
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
