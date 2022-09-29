4ec28990b353c75567aabe9eaa7cb046da96db35
steeven_js <jacques.steeven@gmail.com>
2022-09-29 07:09:23 -0400

    Mise à jour de la db

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -6,7 +6,7 @@
 		e.preventDefault();
 		$('#responsive-nav').toggleClass('active');
 	})
-
+ 
 	// Fix cart dropdown from closing
 	$('.cart-dropdown').on('click', function (e) {
 		e.stopPropagation();
