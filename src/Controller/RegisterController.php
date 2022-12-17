68616e3a8c60db1c327afd962c6cd425fb10c1f8
steeven-js <jacques.steeven@gmail.com>
2022-12-16 23:20:16 -0400

    Vérifications et ajustements

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -2,14 +2,14 @@
 
 namespace App\Controller;
 
-
-use App\Classe\Cart;
+use App\Classe\Mail;
+use App\Entity\Header;
 use App\Entity\User;
 use App\Form\RegisterType;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class RegisterController extends AbstractController
@@ -21,57 +21,43 @@ class RegisterController extends AbstractController
     }
 
     #[Route('/inscription', name: 'app_register')]
-    public function index(Request $request, UserPasswordHasherInterface $encoder, Cart $cart)
+    public function index(Request $request, UserPasswordHasherInterface $encoder)
     {
-        $cart = $cart->get();
 
         $notification = null;
 
-        $user = new User(); 
+        $user = new User();
         $form = $this->createForm(RegisterType::class, $user);
 
-        $form->handleRequest($request); // 1. ecoute de la request
+        $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) { // if 2(ecoute) && 3(check EmailType etc...
-            
-            // dd($user); // 1.
-            
+        if ($form->isSubmitted() && $form->isValid()) {
             $user = $form->getData();
 
-            $serch_email = $this->entityManager->getRepository(User::class)->findOneByEmail($user->getEmail());
+            $search_email = $this->entityManager->getRepository(User::class)->findOneByEmail($user->getEmail());
 
-            if (!$serch_email) {
-                $password = $encoder->hashPassword($user, $user->getPassword());
+            if (!$search_email) {
+                $password = $encoder->hashPassword($user,$user->getPassword());
 
                 $user->setPassword($password);
 
-                // dd($password); // 2.
-                // $doctrine = $this->getDoctrine()->getManager(); $doctrine est passé en parametre il pourra être utilisé plusieurs fois
                 $this->entityManager->persist($user);
-                $this->entityManager->flush();// 4.
+                $this->entityManager->flush();
 
-                // $mail = new Mail();
+                $mail = new Mail();
+                $content = "Bonjour ".$user->getFirstname()."<br/>Bienvenue sur la première boutique dédiée au made in France.<br><br/>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam expedita fugiat ipsa magnam mollitia optio voluptas! Alias, aliquid dicta ducimus exercitationem facilis, incidunt magni, minus natus nihil odio quos sunt?";
+                $mail->send($user->getEmail(), $user->getFirstname(), 'Bienvenue sur La Boutique Française', $content);
 
-                // $content = "Bonjour ".$user->getFirstname()."<br>Bienvenue sur jstech";
-
-                // $mail->send($user->getEmail(), $user->getFirstname(), 'Bienvenue sur la Boutique Française', $content);
-
-                $notification = "Votre inscripition c'est correctement déroulé. Vous pouvez dès à présent vous connecter à votre compte";
-
-            }else {
-
+                $notification = "Votre inscription s'est correctement déroulée. Vous pouvez dès à présent vous connecter à votre compte.";
+            } else {
                 $notification = "L'email que vous avez renseigné existe déjà.";
-                
             }
-
-            // dd($user);
-            
         }
 
         return $this->render('register/index.html.twig', [
             'form' => $form->createView(),
             'notification' => $notification,
-            'cart' => $cart
         ]);
     }
 }
+
